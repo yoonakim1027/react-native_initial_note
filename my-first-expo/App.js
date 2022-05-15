ca2feacebb9dde7e33fa7d9472b8343b5b3b3fcf
yoonakim1027 <20162921@gm.hannam.ac.kr>
2022-05-15 12:52:23 +0900

    새로운 react-native생성

---
+++ my-first-expo/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>새로운 앱 생성 22.05.15</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
