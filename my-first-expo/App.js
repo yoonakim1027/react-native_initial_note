026c755427caefbee25692b3a291e22ebc5b80f2
yoonakim1027 <20162921@gm.hannam.ac.kr>
2022-05-16 23:10:02 +0900

    내장 컴포넌트 Button 생성

--- my-first-expo/App.js
+++ my-first-expo/App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
   console.log("react native app")
   return (
+    <>
     <View style={styles.container}>
       <Text>새로운 앱 생성 22.05.16</Text>
       <Text style={{color:'blue'}}>Hi. react-native</Text>
       <StatusBar style="auto" />
+      <Button title="Button" onPress={() =>alert('click!!')}/>
     </View>
+    </>
+    
   );
 }
 
