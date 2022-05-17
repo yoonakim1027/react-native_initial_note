4b65713168981860e9d9d88d71a7e932da698f02
yoonakim1027 <20162921@gm.hannam.ac.kr>
2022-05-17 15:20:41 +0900

    defaultProps 값을 설정한 뒤 값 적용

--- my-first-expo/App.js
+++ my-first-expo/App.js
@@ -9,8 +9,9 @@ export default function App() {
     <View style={styles.container}>
       <Text>props 버튼 생성 22.05.17</Text>
       <StatusBar style="auto" />
-      <MyButton title="My Button title1" onPress={()=> alert('1')}/>
-      <MyButton title="My Button title2" onPress={()=> alert('2')}/>
+      <MyButton />
+ 
+      <MyButton title="My Button title2" onPress={()=> alert('children')}>children</MyButton>
 
     </View>
     </>
--- my-first-expo/src/MyButton.js
+++ my-first-expo/src/MyButton.js
@@ -1,12 +1,13 @@
 import react from "react";
 import { TouchableOpacity, View, Text } from "react-native";
 
-const MyButton =props => {
-    console.log(props)
+const MyButton =({title="title", onPress=()=>{}, children}) => {
     return (
-        <TouchableOpacity onPress={props.onPress}>
+        <TouchableOpacity onPress={onPress}>
             <View style={{margin:100 , backgroundColor:'red', padding:10}}>
-                <Text style={{fontSize:20, color:'white'}}>{props.title}</Text>
+                <Text style={{fontSize:20, color:'white'}}>
+                    {/* children이 있으면 children, 없으면 title 출력 */}
+                    {children || title}</Text>
             </View>
         </TouchableOpacity>
     )
@@ -18,5 +19,11 @@ const MyButton =props => {
 // 이렇게 재사용하면서 컴포넌트를 사용할 수 있음 
 
 
+MyButton.defaultProps = {
+    title : 'default',
+    onPress : () => alert('default')
+
+
+}
 
 export default MyButton;
