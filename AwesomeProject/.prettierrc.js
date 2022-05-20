6379967aac88c71e0b43cbda1292bd58c36ec00f
yoonakim1027 <20162921@gm.hannam.ac.kr>
2022-05-20 15:50:43 +0900

    EsLint 적용

--- AwesomeProject/.prettierrc.js
+++ AwesomeProject/.prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  arrowParens: 'avoid',
+
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  arrowParens: "always",
+  orderedImports: true,
+  printWidth: 80,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false
 };
---
+++ AwesomeProject/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                My First React native CLI
+            </Text>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+
+    container : {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor : '#ffffff',
+    },
+    title : {
+        fontSize : 30,
+    },
+
+
+})
+
+export default App;
