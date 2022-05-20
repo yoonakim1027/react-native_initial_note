0741677f626207b90c562188ef1cadefa04ab3ee
yoonakim1027 <20162921@gm.hannam.ac.kr>
2022-05-20 16:16:43 +0900

    AND 연산자, OR연산자를 이용 : 조건에 해당되는 값 출력

--- react-native-component/App.js
+++ react-native-component/App.js
@@ -5,8 +5,22 @@ export default function App() {
   const name = 'chorong';
   return (
     <View style={styles.container}>
-      <Text>
-        My name is {name === 'yoona' ? 'yoona' : 'react native'}</Text>
+{/* AND(&&) 연산자를 사용
+조건이 참일 때 왼쪽출력 : 조건이 거짓일 때 오른쪽 출력       */}
+
+{/* OR (||) 연산자를 사용
+앞의 조건이 거짓인 경우 내용이 나타나고, 조건이 참일 경우 나타나지 않음  */}
+      {name === 'chorong' && (
+        <Text style={styles.text}>
+          My name is chorong
+        </Text>
+      )}
+      {name !== 'chorong' && (
+        <Text style={styles.text}>
+          My name is react native
+        </Text>
+      )}
+      
       <StatusBar style="auto" />
     </View>
   );
